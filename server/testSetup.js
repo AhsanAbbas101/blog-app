c9156a7e4b4e8898e03c523a5a32a4c84f61b159
Ahsan Abbas <ahsanabbas468@gmail.com>
2025-03-09 19:43:23 +0200

    part11 exercise 11.20 - .env test 2

--- server/testSetup.js
+++ server/testSetup.js
@@ -1,3 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('module-alias/register')
 require('dotenv').config()
+
+// eslint-disable-next-line no-console
+console.log(`Hello ${process.env.HELLO}`)
